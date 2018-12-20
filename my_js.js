56aa7f3f396cb44f5a67e9a57acec3420ed0eae0
Swaraj Somarajan <swarajsomarajan@gmail.com>
2018-12-20 08:22:13 +0530

    Update my_js.js
    adding the function with defineProperty instead of directly extending the Object.prototype.

--- my_js.js
+++ my_js.js
@@ -1,29 +1,13 @@
 "use strict";
-Object.prototype.nativeData = function(attribute, value) {
-	let elements = this;
-	if(attribute === undefined && value === undefined)
-	{
-		let dataSet = {};
-		if(typeof elements === 'object')
+Object.defineProperty(Object.prototype, "nativeData", {
+	value     : function(attribute, value) {
+		let elements = this;
+		if(attribute === undefined && value === undefined)
 		{
-			if(window.jQuery !== undefined && elements instanceof jQuery)
+			let dataSet = {};
+			if(typeof elements === 'object')
 			{
-				dataSet = [];
-				for(const element of elements)
-				{
-					if(element.nodeType === 1)
-					{
-						dataSet = [...dataSet, Object.assign({}, element.dataset)];
-					}
-				}
-			}
-			else
-			{
-				if(elements.nodeType === 1)
-				{
-					dataSet = Object.assign({}, elements.dataset);
-				}
-				else if(elements.constructor.name === 'NodeList')
+				if(window.jQuery !== undefined && elements instanceof jQuery)
 				{
 					dataSet = [];
 					for(const element of elements)
@@ -34,49 +18,32 @@ Object.prototype.nativeData = function(attribute, value) {
 						}
 					}
 				}
+				else
+				{
+					if(elements.nodeType === 1)
+					{
+						dataSet = Object.assign({}, elements.dataset);
+					}
+					else if(elements.constructor.name === 'NodeList')
+					{
+						dataSet = [];
+						for(const element of elements)
+						{
+							if(element.nodeType === 1)
+							{
+								dataSet = [...dataSet, Object.assign({}, element.dataset)];
+							}
+						}
+					}
+				}
 			}
+			return dataSet;
 		}
-		return dataSet;
-	}
-	else
-	{
-		if(typeof elements === 'object')
+		else
 		{
-			if(window.jQuery !== undefined && elements instanceof jQuery)
+			if(typeof elements === 'object')
 			{
-				for(const element of elements)
-				{
-					if(element.nodeType === 1)
-					{
-						if(typeof attribute === 'string')
-						{
-							element.setAttribute(`data-${attribute}`, (typeof value === "function" ? value(element) : value));
-						}
-						else if(typeof attribute === 'object')
-						{
-							Object.keys(attribute).map((property) => {
-								element.setAttribute(`data-${property}`, (typeof attribute[property] === "function" ? attribute[property](element) : attribute[property]));
-							});
-						}
-					}
-				}
-			}
-			else
-			{
-				if(elements.nodeType === 1)
-				{
-					if(typeof attribute === 'string')
-					{
-						elements.setAttribute(`data-${attribute}`, (typeof value === "function" ? value(elements) : value));
-					}
-					else if(typeof attribute === 'object')
-					{
-						Object.keys(attribute).map((property) => {
-							elements.setAttribute(`data-${property}`, (typeof attribute[property] === "function" ? attribute[property](elements) : attribute[property]));
-						});
-					}
-				}
-				else if(elements.constructor.name === 'NodeList')
+				if(window.jQuery !== undefined && elements instanceof jQuery)
 				{
 					for(const element of elements)
 					{
@@ -95,7 +62,43 @@ Object.prototype.nativeData = function(attribute, value) {
 						}
 					}
 				}
+				else
+				{
+					if(elements.nodeType === 1)
+					{
+						if(typeof attribute === 'string')
+						{
+							elements.setAttribute(`data-${attribute}`, (typeof value === "function" ? value(elements) : value));
+						}
+						else if(typeof attribute === 'object')
+						{
+							Object.keys(attribute).map((property) => {
+								elements.setAttribute(`data-${property}`, (typeof attribute[property] === "function" ? attribute[property](elements) : attribute[property]));
+							});
+						}
+					}
+					else if(elements.constructor.name === 'NodeList')
+					{
+						for(const element of elements)
+						{
+							if(element.nodeType === 1)
+							{
+								if(typeof attribute === 'string')
+								{
+									element.setAttribute(`data-${attribute}`, (typeof value === "function" ? value(element) : value));
+								}
+								else if(typeof attribute === 'object')
+								{
+									Object.keys(attribute).map((property) => {
+										element.setAttribute(`data-${property}`, (typeof attribute[property] === "function" ? attribute[property](element) : attribute[property]));
+									});
+								}
+							}
+						}
+					}
+				}
 			}
 		}
-	}
-};
+	},
+	enumerable: false
+});
